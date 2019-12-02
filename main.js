d918277df7c736af3f834652e419b59c8476008a
Colin Jaffe <balloonasaurus@gmail.com>
2019-12-02 13:42:20 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,85 @@
+const makeDino = function(species, period, carnivore, extinct = false) {
+  return {
+    species,
+    period,
+    carnivore,
+    extinct
+  }
+}
+
+const makeSingular = function(dinosaur) {
+  const {species, period, carnivore, extinct} = dinosaur;
+  const newDino = makeDino(species, period, carnivore, extinct);
+
+  if (species.endsWith('us')) {
+    newDino.species = species.slice(0, -2);
+  }
+
+  return newDino;
+}
+
+const truncateSpecies = function(dinosaur) {
+  const {species, period, carnivore, extinct} = dinosaur;
+  const newDino = makeDino(species, period, carnivore, extinct);
+
+  if (species.length > 10) {
+    newDino.species = species.slice(0, 7) + '...';
+  }
+
+  return newDino;
+}
+
+const makeExtinct = function(dinosaur) {
+  const {species, period, carnivore} = dinosaur;
+  const newDino = makeDino(species, period, carnivore, true);
+
+  return newDino;
+
+  // Alternately (but including line 1 of this function)
+  
+  // return {
+  //   species,
+  //   period,
+  //   carnivore,
+  //   extinct: true
+  // }
+}
+
+const isCarnivore = function(dinosaur) {
+  return dinosaur.carnivore === true;
+
+  // Or, more simply:
+  // return dinosaur.carnivore
+}
+
+const isExtinct = function(dinosaur) {
+  return dinosaur.extinct === true;
+
+  // Or, more simply:
+  // return dinosaur.extinct
+}
+
+const isTriassic = function(dinosaur) {
+  return dinosaur.period === 'Triassic';
+}
+
+const isJurassic = function(dinosaur) {
+  return dinosaur.period === 'Jurassic';
+}
+
+const isCretaceous = function(dinosaur) {
+  return dinosaur.period === 'Cretaceous';
+}
+
+
+module.exports = {
+  makeDino,
+  makeSingular,
+  truncateSpecies,
+  makeExtinct,
+  isCarnivore,
+  isExtinct,
+  isTriassic,
+  isJurassic,
+  isCretaceous,
+}
